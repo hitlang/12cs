387b3e5ee72e0b429bf7ad6750f5a44d0fa00fab
liulang <newlang@126.com>
2020-12-10 15:38:25 +0800

    修改了

--- day4/ex1.py
+++ day4/ex1.py
@@ -5,9 +5,19 @@ import time
 '''
 统计： 函数的调用执行时间
 '''
+def timethis(fn):
+    def inner(*args, **kwargs):
+
+        print("----开始调用函数-----")
+        start = time.time()
+        fn(*args,**kwargs)
+        end = time.time()
+        print("----函数调用结束-----，执行时间{}s".format(end -start))
 
 
+    return inner
 
+@timethis
 def foo1(name, password):
     # if name == "kevin" and password == "123456":
         ''''
@@ -17,7 +27,7 @@ def foo1(name, password):
         print("---执行业务操作1----------------")
         pass
 
-
+@timethis
 def foo2(name, password):
     # if name == "kevin" and password == "123456":
         ''''
@@ -27,14 +37,13 @@ def foo2(name, password):
         print("---执行业务操作2----------------")
         pass
 
+@timethis
+def foo3():
+    print("---执行业务操作3----------------")
+
+
 if __name__ == '__main__':
 
-    start = time.time()
-
-    foo2("ddd","111")
-
-    end = time.time()
-
-    print(end - start)
+    foo3()
 
     pass
