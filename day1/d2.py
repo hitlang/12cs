5d9e0a8d511a2c1cb5039595ec736c4ab81d6f22
liulang <newlang@126.com>
2021-02-22 20:26:57 +0800

    更新

--- day1/d2.py
+++
@@ -1,7 +0,0 @@
-#-*-coding:utf-8 -*-
-#!/usr/bin/python3
-# @Author:liulang
-
-
-# 这个特别的变量，值是当前py文件的路径
-print(__file__) # G:/虫师/python/12_terms/12cs/day1/d2.py
--- day1/d5.py
+++
@@ -1,9 +0,0 @@
-#-*-coding:utf-8 -*-
-#!/usr/bin/python3
-# @Author:liulang
-"""
-注释： 这个我的第一个python程序
-
-"""
-print(__doc__) #表示当前py文件中的，注释部分
-
